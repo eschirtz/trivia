53a5eed71b786a0d0b817d6870121efa84733311
Eric Schirtzinger <eschirtzinger@gmail.com>
2021-06-20 12:20:24 -0500

    feat: questions

--- src/views/questions.ts
+++ src/views/questions.ts
@@ -1,16 +1,65 @@
 const questions: { text: string, answer: 'eric' | 'ian' | 'adam' }[] = [
-  {
-    text: 'Who is the oldest son?',
-    answer: 'eric'
-  },
-  {
-    text: 'Who is the youngest son?',
-    answer: 'adam'
-  },
-  {
-    text: 'Who is the middle son?',
-    answer: 'ian'
-  }
+ {
+   text: "_____ is to Ian as Tony is to Tom.",
+   answer: "eric"
+ },
+ {
+   text: "Which one of the truimphet trio loved monkeys so much he wanted a brother named after Tarzan?",
+   answer: "ian"
+ },
+ {
+   text: "Instead of having ketchup with their fries, _____ calls it having fries with their ketchup.",
+   answer: "adam"
+   
+ },
+ {
+   text: "This amigo has friends named Julia, Kevin, Rachel, and Tony.",
+   answer: "ian"
+ },
+ {
+   text: "To be as timeless as Loki, which brother is most likely to live through 3 centuries?",
+   answer: "ian"
+ },
+ {
+   text: "Who is known to have the ability to cut through the strongest rock on earth with their left nip?",
+   answer: "eric"
+ },
+ {
+   text: "Who can \"Call Kevin\" up on the phone at any time?",
+   answer: 'adam'
+ },
+ {
+   text: "Which of these male specimens has profited big time due to his delicious dance moves?",
+   answer: "adam"
+ },
+ {
+   text: "He was the fierce, the adventurous, the bold, BAG MAN! for hollows eve.",
+   answer: "eric"
+ },
+ {
+   text: "Hide & seek is a lovely game; however, this boy once took it too far after almost getting hit by a car.",
+   answer: 'adam'
+ },
+ {
+   text: "Sandwiches are delicious, but this boy must have had a truly special sandwich when he discovered his career calling.",
+   answer: 'ian'
+ },
+ {
+   text: "Not only known for taking long \"number two's\", this person has obtained up to two companies.",
+   answer: 'eric'
+ },
+ {
+   text: "While the most attractive Schirtzinger is still up to debate, this boy is definitely the \"hottest\" when he's outside scoopin' poop.",
+   answer: 'eric'
+ },
+ {
+   text: "Generally this boy is happy and positive, but perhaps a better way to describe him is \"chippy\" especially when he's working on his golf game.",
+   answer: 'ian'
+ },
+ {
+   text: "In the summer this person changes so fast his friends call him the \"Chameleon\".",
+   answer: 'adam'
+ }
 ]
 
 export default questions
