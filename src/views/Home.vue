7641802140d0d590591109d74f22133ad0310153
Eric Schirtzinger <eschirtzinger@gmail.com>
2021-06-20 11:45:07 -0500

    technically works?

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,21 @@
 <template>
   <button v-if="!gameURL" @click="createGame">Create game</button>
   <h6 v-else>{{ gameURL }}</h6>
-  <h4 v-for="player in players" :key="player.id">{{ player.name }}: {{player.score}}</h4>
+  <h4 v-for="player in players" :key="player.id">
+    {{ player.name }}: {{ player.score }}
+  </h4>
   <button v-if="questionIndex === -1 && gameURL" @click="nextQuestion">
     Start Game
   </button>
+  <div v-if="questionIndex !== -1">
+    {{ question.text }}
+  </div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, ref, Ref } from "vue";
 import firebase from "firebase/app";
+import questions from "./questions";
 
 export default defineComponent({
   setup() {
@@ -19,6 +25,51 @@ export default defineComponent({
     }>;
     const questionIndex = ref(-1);
 
+    const players = computed(() => {
+      const list = [];
+      for (const playerid in playerData.value) {
+        // @ts-ignore
+        const player = playerData.value[playerid];
+        list.push({
+          id: playerid,
+          name: player.name,
+          score: player.score,
+        });
+      }
+      return list;
+    });
+
+    const gameURL = computed(() => {
+      if (!gameid.value) return "";
+      const url = `${window.location.origin}/game/${gameid.value}`;
+      return url;
+    });
+
+    const question = computed(() => {
+      return questions[questionIndex.value];
+    });
+
+    function endGame() {
+      let highestScore = Number.MIN_VALUE;
+      let winner = 'No one';
+      for (const playerid in playerData.value) {
+        const player = playerData.value[playerid];
+        if (player.score > highestScore) {
+          highestScore = player.score;
+          winner = player.name;
+        }
+      }
+      alert(winner + ' wins!');
+    }
+
+    function nextQuestion() {
+      if (questionIndex.value >= questions.length - 1) {
+        endGame();
+      } else {
+        questionIndex.value += 1;
+      }
+    }
+
     function createGame() {
       const gameRef = firebase
         .database()
@@ -27,49 +78,30 @@ export default defineComponent({
       gameid.value = gameRef.key ?? "";
       // Listen to changes to the game
       gameRef.child("players").on("child_added", (snap) => {
-        console.log(snap.val());        
+        console.log(snap.val());
         playerData.value[snap.key as string] = {
           name: snap.val().name,
           score: 0,
         };
       });
-      
-      gameRef.child('answers').on('child_added', snap => {
-        console.log(snap.val());  
-        const answer = snap.val() as {choice: 'eric' | 'ian' | 'adam', playerid: string}  
+
+      gameRef.child("answers").on("child_added", (snap) => {
+        console.log(snap.val());
+        const answer = snap.val() as {
+          choice: "eric" | "ian" | "adam";
+          playerid: string;
+        };
         // Check if answer is correct
-        if(true)    {
+        if (question.value.answer === answer.choice) {
           playerData.value[answer.playerid].score += 1;
+          console.log("YOU ARE RIGHT!");
+        } else {
+          playerData.value[answer.playerid].score -= 1;
+          console.error("YOU ARE WRONG!");
         }
-      })
+        setTimeout(nextQuestion, 3000); // wait 5 seconds before advancing to next question        
+      });
     }
-
-    function nextQuestion() {
-      questionIndex.value += 1;
-      for(const player of players.value) {
-        playerData.value[player.id].score += 2
-      }
-    }
-
-    const players = computed(() => {
-      const list = [];
-      for (const playerid in playerData.value) {
-        // @ts-ignore
-        const player = playerData.value[playerid];
-        list.push({          
-          id: playerid,
-          name: player.name,
-          score: player.score,          
-        });
-      }
-      return list;
-    });
-    
-    const gameURL = computed(() => {
-      if (!gameid.value) return "";
-      const url = `${window.location.origin}/game/${gameid.value}`;
-      return url;
-    });
 
     return {
       createGame,
@@ -78,6 +110,7 @@ export default defineComponent({
       gameURL,
       players,
       questionIndex,
+      question,
     };
   },
 });
---
+++ src/views/questions.ts
@@ -0,0 +1,16 @@
+const questions: { text: string, answer: 'eric' | 'ian' | 'adam' }[] = [
+  {
+    text: 'Who is the oldest son?',
+    answer: 'eric'
+  },
+  {
+    text: 'Who is the youngest son?',
+    answer: 'adam'
+  },
+  {
+    text: 'Who is the middle son?',
+    answer: 'ian'
+  }
+]
+
+export default questions
